8b1bef1f411041cbe787f3c67b9abb1edd6da53f
Mark Heard <mark@clearlyfiltered.com>
2019-02-18 15:16:44 -0800

    set up basic shipstation api call

--- controllers/html_controller.js
+++ controllers/html_controller.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 var express = require('express');
 var router = express.Router();
 var request = require('request');
@@ -6,14 +7,32 @@ router.get('/', function (req, res) {
     res.render('index');
 });
 
-router.get('/shipments', function (req, res) {
+function testCall(res ,cb){
+    var authString = `${process.env.SSAPIKey}:${process.env.SSAPISecret}`; 
+    var cipherString = Buffer.from(authString).toString('base64');
+    var authString = `Basic ${cipherString}`;
     var options = {
-        url: 'https://api.github.com/repos/request/request',
+        url: 'https://ssapi.shipstation.com/orders?createDateStart=2019-02-17%2023:59:59',
         headers: {
-          'User-Agent': 'request'
+          Authorization: authString 
         }
       };
-    res.render('shipments');
+      function callback(error, response, body) {
+        if (!error && response.statusCode == 200) {
+          var info = JSON.parse(body);
+          console.log(info);
+          res.render('shipments', info);
+        }
+      }
+      request(options, callback);
+      
+}
+
+router.get('/shipments', function (req, res) {
+
+    testCall(res);
+    
+    
 });
 
 router.get('/tickets', function (req, res) {
--- index.js
+++ index.js
@@ -1,21 +1,16 @@
+require('dotenv').config();
 var express = require('express');
 var exphbs  = require('express-handlebars');
 var PORT = process.env.PORT || 3000; 
 var app = express();
 var models = require("./models");
 var htmlRoutes = require("./controllers/html_controller");
-
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
- 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
-
 app.use(express.static("public"));
-
 app.use(htmlRoutes);
-
-
 models.sequelize.sync().then(function(){
     app.listen(PORT, function(){
         console.log(`Server now listening on port: ${PORT}`);
