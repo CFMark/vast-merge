3dd7539206a2d2c66b8893c0686707fb0dbda711
Mark Heard <mark@clearlyfiltered.com>
2019-02-18 14:32:21 -0800

    added .env, .gitignore, request module, axios module

--- controllers/html_controller.js
+++ controllers/html_controller.js
@@ -1,8 +1,23 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request');
 
 router.get('/', function (req, res) {
     res.render('index');
+});
+
+router.get('/shipments', function (req, res) {
+    var options = {
+        url: 'https://api.github.com/repos/request/request',
+        headers: {
+          'User-Agent': 'request'
+        }
+      };
+    res.render('shipments');
+});
+
+router.get('/tickets', function (req, res) {
+    res.render('tickets');
 });
 
 module.exports = router;
