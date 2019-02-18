95a88b4fd114d95f67811cd2e73b5e85e2b3f293
Mark Heard <mark@clearlyfiltered.com>
2019-02-18 11:40:45 -0800

    worte initial server config

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 var express = require('express');
 var exphbs  = require('express-handlebars');
- 
+var PORT = process.env.PORT || 3000; 
 var app = express();
+var models = require("./models");
+
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
  
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
@@ -9,5 +13,9 @@ app.set('view engine', 'handlebars');
 app.get('/', function (req, res) {
     res.render('home');
 });
- 
-app.listen(3000);
+
+models.sequelize.sync().then(function(){
+    app.listen(PORT, function(){
+        console.log(`Server now listening on port: ${PORT}`);
+    });
+});
