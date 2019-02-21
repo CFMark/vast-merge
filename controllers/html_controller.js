0c43d2497a14d35902a5a13357ac0c2ed48eee7d
Mark Heard <mark@clearlyfiltered.com>
2019-02-21 12:31:48 -0800

    added name normaliztion for todays shipments

--- controllers/html_controller.js
+++ controllers/html_controller.js
@@ -1,9 +1,9 @@
 require('dotenv').config();
 var express = require('express');
 var router = express.Router();
-var request = require('request');
 var shipStationAPI = require('../apis/shipstation');
 var zendeskAPI = require('../apis/zendesk');
+var names = require('../logic/normalize_name');
 
 router.get('/', function (req, res) {
     res.render('index');
@@ -18,14 +18,50 @@ router.get('/shipments', function (req, res) {
   }
 
   shipStationAPI.get('shipments',reqParams, function(data){
-    var shipments ={shipments: []};
+    var shipments ={
+      shipments: [],
+      lineItems: []
+    };
+    var lineItems = {lineItems: []};
+
     for(var i=0; i<data.shipments.length; i++){
       var shipment = data.shipments[i];
       if( shipment.advancedOptions.storeId !== 172911 && shipment.warehouseId === 227247 && shipment.isReturnLabel === false){
         shipments.shipments.push(data.shipments[i]);
+        var orderNumber = shipment.orderNumber;
+        var customerEmail = shipment.customerEmail;
+        var recipientName = shipment.shipTo.name;
+        var newlineItem, name;
+        if(shipment.shipmentItems.length === 0){
+          name = names.normalize(shipment.shipmentItems[0].name);
+          newlineItem = {
+            orderNumber: orderNumber,
+            customerEmail: customerEmail,
+            recipientName: recipientName,
+            productName: name,
+            productQty: shipment.shipmentItems[0].quantity
+          };
+          console.log("condition valid!!!")
+          shipments.lineItems.push(newlineItem);
+        } else {
+          for(var j = 0; j<shipment.shipmentItems.length; j++){
+            name = names.normalize(shipment.shipmentItems[j].name);
+            newlineItem = {
+              orderNumber: orderNumber,
+              customerEmail: customerEmail,
+              recipientName: recipientName,
+              productName: name,
+              productQty: shipment.shipmentItems[j].quantity
+            };
+            shipments.lineItems.push(newlineItem);
+          }
+        }
+
       }
     }
+
     //console.log(shipments);
+    //console.log(shipments.lineItems);
     res.render('shipments', shipments);
   });
 });
--- logic/normalize_name.js
+++ logic/normalize_name.js
@@ -1,12 +1,82 @@
 var normalize = {
     normalize: function(name){
         switch(name){
-            case 'piter Filter': 
+            //SINGLE PITCHER FILTER
+            case 'VIP Club (Clean Water Pitcher Filter)': 
             name = 'Pitcher Filter';
-            break;
+            return name;
+            case 'Clean Water Pitcher Replacement Filter  15% Off Auto renew': 
+            name = 'Pitcher Filter';
+            return name;
+            case 'Clean Water Pitcher Replacement Filter  15.00% Off Auto renew': 
+            name = 'Pitcher Filter';
+            return name;
+            case 'Clean Water Pitcher Replacement Filter': 
+            name = 'Pitcher Filter';
+            return name;
+            case 'Clean Water Pitcher Replacement Filter - VIP': 
+            name = 'Pitcher Filter';
+            return name;
+            case 'Clean Water Pitcher Replacement Filter - VIP (Ships every 2 Months)': 
+            name = 'Pitcher Filter';
+            return name;
+            case 'Clean Water Pitcher Replacement Filter - VIP (Ships every 3 Months)': 
+            name = 'Pitcher Filter';
+            return name;
+            case 'Clean Water Pitcher Replacement Filter - VIP (Ships every 4 Months)': 
+            name = 'Pitcher Filter';
+            return name;
+            //PITCHER FILTER 3 PACK
+            case 'Pitcher Filter 3-Pack  15% Off Auto renew': 
+            name = 'Pitcher Filter 3 Pack';
+            return name;
+            case 'Clean Water Pitcher Replacement Filter - 3-Pack': 
+            name = 'Pitcher Filter 3 Pack';
+            return name;
+            case 'Clean Water Pitcher Replacement Filter - 3-Pack  15.00% Off Auto renew (Ships every 6 Months)': 
+            name = 'Pitcher Filter 3 Pack';
+            return name;
+            case 'Clean Water Pitcher Replacement Filter - 3-Pack  15.00% Off Auto renew (Ships every 9 Months)': 
+            name = 'Pitcher Filter 3 Pack';
+            return name;
+            case 'Clean Water Pitcher Replacement Filter - 3-Pack  15.00% Off Auto renew (Ships every 12 Months)': 
+            name = 'Pitcher Filter 3 Pack';
+            return name;
+            //PITCHER FILTER 6 PACK
+            case 'Clean Water Pitcher Replacement Filter - 6 Pack  15.00% Off Auto renew': 
+            name = 'Pitcher Filter 6 Pack';
+            return name;
+            //PITCHER
+            case 'Water Pitcher with Affinity Filtration Technology': 
+            name = 'Pitcher';
+            return name;
+            case 'Water Pitcher with Affinity Filtration Technology  15.00% Off Auto renew': 
+            name = 'Pitcher';
+            return name;
+            //BUNDLE: PITCHER AND 1 FILTER
+            //BUNDLE: PITCHER AND 3 FILTER
+            //BUNDLE: PITCHER AND 6 FILTER
+            //PITCHER BODY
+            case 'Water Pitcher - Body Only': 
+            name = 'Pitcher Body';
+            return name;
+            //STAINLESS STEEL BOTTLE
+            //BOTTLE FILTER
+            case 'Bottle Replacement Filter - VIP': 
+            name = 'Bottle Filter';
+            return name;
+            case 'Stainless Steel Filter Bottle Replacement Filter  15% Off Auto renew': 
+            name = 'Bottle Filter';
+            return name;
+            case 'Stainless Steel Bottle With Filter, VIP Club (Advanced Bottle Filter)': 
+            name = 'Bottle Filter';
+            return name;
+
+
             default:
-            name = 'Match not Found';
-            break;
+            return name;
         }
     }
 }
+
+module.exports = normalize;
