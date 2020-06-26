b5806f8d3527b8f53f776e5ae6f6503f0e276fde
CFMark <mark@clearlyfiltered.com>
2020-06-26 15:48:49 -0700

    making sure up to date

--- controllers/html_controller.js
+++ controllers/html_controller.js
@@ -5,21 +5,30 @@ var shipStationAPI = require('../apis/shipstation');
 var zendeskAPI = require('../apis/zendesk');
 var names = require('../logic/normalize_name');
 
+//index end point
 router.get('/', function (req, res) {
   res.render('index');
 });
 
+//shipments end point
 router.get('/shipments/', function (req, res) {
   var date = req.params.date;
 
-  var today = new Date().toISOString().split("T")[0];
+  //if no date has been provided-- use today's date
+  if(date === undefined){
+    //set variable to today's date and remove the time data
+    var today = new Date().toISOString().split("T")[0];
+  }
 
+  //here we set up a query object to pass to the Shipstation API controller method we imported 
   var reqParams = {
     'shipDateStart': `${today}%2000:00:00`,
     'includeShipmentItems': true,
   }
 
+  //here the Shipstation API controller is invoked
   shipStationAPI.get('shipments', reqParams, function (data) {
+    //create a data object to store shipment information
     var shipments = {
       shipments: [],
       lineItems: [],
@@ -28,20 +37,35 @@ router.get('/shipments/', function (req, res) {
       
     };
 
+    //loop through the data object
     for (var i = 0; i < data.shipments.length; i++) {
+      //set variable to current shipment in iteration
       var shipment = data.shipments[i];
+      //filter the shipments by store and remove the returns
       if (shipment.advancedOptions.storeId !== 172911 && shipment.warehouseId === 227247 && shipment.isReturnLabel === false) {
+
+        //push a copy of the original shipment to the shipments array on the data object for later analysis
         shipments.shipments.push(data.shipments[i]);
+
+        //set variable for the shipments order number
         var orderNumber = shipment.orderNumber;
+        //set variable for the shipments customers email
         var customerEmail = shipment.customerEmail;
+        //set a variable to the recipient name
         var recipientName = shipment.shipTo.name;
+        //declare two empty variables for later use
         var newlineItem, name;
 
+        //loop through the shipment items array
         for (var j = 0; j < shipment.shipmentItems.length; j++) {
+          //if the shipment item is not a subscription only item
           if(shipment.shipmentItems[j].options !== null && shipment.shipmentItems[j].options[0].name === 'charge_delay' && shipment.shipmentItems[j].unitPrice === 0){
+            //log the subscription item to the console
             console.log('subscription only');
           } else {
+            //if the shipment is not a subscription item 
             name = names.normalize(shipment.shipmentItems[j].name);
+            //set the newlineItem to an object that contains
             newlineItem = {
               orderNumber: orderNumber,
               customerEmail: customerEmail,
@@ -49,6 +73,7 @@ router.get('/shipments/', function (req, res) {
               productName: name,
               productQty: shipment.shipmentItems[j].quantity
             };
+            //push to the shipment lineitems array for further aggregation
             shipments.lineItems.push(newlineItem);
           }
         }
@@ -57,7 +82,10 @@ router.get('/shipments/', function (req, res) {
 
     for(var i = 0; i < shipments.lineItems.length; i++){
       var countKeys = Object.keys(shipments.counts);
-      var shipSKUName = `${shipments.lineItems[i].productName}x${shipments.lineItems[i].productQty}`;
+      var shipSKUName = `${shipments.lineItems[i].productName} x ${shipments.lineItems[i].productQty}`;
+      //insert logic to assess SKU name and correct it if needed
+      //EXAMPLE
+      //3 Pitcher Filter sold together should be counted as a 3 Pack
       var exists = countKeys.indexOf(shipSKUName);
       if(exists<0){
         shipments.counts[shipSKUName] = 1;
@@ -74,9 +102,11 @@ router.get('/shipments/', function (req, res) {
       };
       shipments.countsArr.push(newCount);
     }
+    shipments.date = today;
 
-    console.log(shipments.counts);
-    console.log(shipments.countsArr);
+    //console.log(shipments.counts);
+    //console.log(shipments.countsArr);
+    //console.log(shipments.date)
 
 
     res.render('shipments', shipments);
--- logic/normalize_name.js
+++ logic/normalize_name.js
@@ -17,6 +17,21 @@ var normalize = {
             case 'Clean Water Pitcher Replacement Filter - VIP': 
             name = 'Pitcher Filter';
             return name;
+            case 'Clean Water Pitcher Replacement Filter  10.00% Off with Subscription (Ships every 2 Months)': 
+            name = 'Pitcher Filter';
+            return name;
+            case 'Clean Water Pitcher Replacement Filter  10.00% Off with Subscription (Ships every 3 Months)': 
+            name = 'Pitcher Filter';
+            return name;
+            case 'Clean Water Pitcher Replacement Filter  10.00% Off with Subscription (Ships every 4 Months)': 
+            name = 'Pitcher Filter';
+            return name;
+            case 'Clean Water Pitcher Replacement Filter  10.00% Off with Subscription (Ships every 5 Months)': 
+            name = 'Pitcher Filter';
+            return name;
+            case 'Clean Water Pitcher Replacement Filter  10.00% Off with Subscription (Ships every 6 Months)': 
+            name = 'Pitcher Filter';
+            return name;
             case 'Clean Water Pitcher Replacement Filter - VIP (Ships every 2 Months)': 
             name = 'Pitcher Filter';
             return name;
@@ -26,11 +41,51 @@ var normalize = {
             case 'Clean Water Pitcher Replacement Filter - VIP (Ships every 4 Months)': 
             name = 'Pitcher Filter';
             return name;
+            case 'Complimentary Pitcher Filter': 
+            name = 'Pitcher Filter';
+            return name;
+            case 'Clean Water Replacement Filter - Default': 
+            name = 'Pitcher Filter';
+            return name;
+            case 'Clean Water Pitcher Replacement Filter  10.00% Off with Subscription': 
+            name = 'Pitcher Filter';
+            return name;
+            case 'Clean Water Pitcher Replacement Filter  10.00% Off with Filter Auto-Ship Subscription': 
+            name = 'Pitcher Filter';
+            return name;
+
+            case 'Clean Water Pitcher Replacement Filter  10.00% Off with Filter Auto-Ship Subscription (Ships every 2 Months)': 
+            name = 'Pitcher Filter';
+            return name;
+
+            case 'Clean Water Pitcher Replacement Filter  10.00% Off with Filter Auto-Ship Subscription (Ships every 3 Months)': 
+            name = 'Pitcher Filter';
+            return name;
+
+            case 'Clean Water Pitcher Replacement Filter  10.00% Off with Filter Auto-Ship Subscription (Ships every 4 Months)': 
+            name = 'Pitcher Filter';
+            return name;
+            
+            case 'Clean Water Pitcher Replacement Filter  10.00% Off with Filter Auto-Ship Subscription (Ships every 5 Months)': 
+            name = 'Pitcher Filter';
+            return name;
+
+            case 'Clean Water Pitcher Replacement Filter  10.00% Off with Filter Auto-Ship Subscription (Ships every 6 Months)': 
+            name = 'Pitcher Filter';
+            return name;
+
+
             //PITCHER FILTER 3 PACK
             case 'Pitcher Filter 3-Pack  15% Off Auto renew': 
             name = 'Pitcher Filter 3 Pack';
             return name;
             case 'Clean Water Pitcher Replacement Filter - 3-Pack': 
+            name = 'Pitcher Filter 3 Pack';
+            return name;
+            case 'Clean Water Pitcher Replacement Filter - 3-Pack  10.00% Off with Auto-Ship (Ships every 6 Months)': 
+            name = 'Pitcher Filter 3 Pack';
+            return name;
+            case 'Clean Water Pitcher Replacement Filter - 3-Pack  15.00% Off Auto renew': 
             name = 'Pitcher Filter 3 Pack';
             return name;
             case 'Clean Water Pitcher Replacement Filter - 3-Pack  15.00% Off Auto renew (Ships every 6 Months)': 
@@ -42,10 +97,19 @@ var normalize = {
             case 'Clean Water Pitcher Replacement Filter - 3-Pack  15.00% Off Auto renew (Ships every 12 Months)': 
             name = 'Pitcher Filter 3 Pack';
             return name;
+            case 'Clean Water Pitcher Replacement Filter - 3-Pack  10.00% Off with Auto-Ship': 
+            name = 'Pitcher Filter 3 Pack';
+            return name;
+
             //PITCHER FILTER 6 PACK
             case 'Clean Water Pitcher Replacement Filter - 6 Pack  15.00% Off Auto renew': 
             name = 'Pitcher Filter 6 Pack';
             return name;
+            case 'Clean Water Pitcher Replacement Filter - 6 Pack': 
+            name = 'Pitcher Filter 6 Pack';
+            return name;
+
+
             //PITCHER
             case 'Water Pitcher with Affinity Filtration Technology': 
             name = 'Pitcher';
@@ -53,6 +117,44 @@ var normalize = {
             case 'Water Pitcher with Affinity Filtration Technology  15.00% Off Auto renew': 
             name = 'Pitcher';
             return name;
+            case 'Water Pitcher with Affinity Filtration Technology  10.00% Off with Filter Auto-Ship (Ships every 2 Months)': 
+            name = 'Pitcher';
+            return name;
+            case 'Water Pitcher with Affinity Filtration Technology  10.00% Off with Filter Auto-Ship (Ships every 3 Months)': 
+            name = 'Pitcher';
+            return name;
+            case 'Water Pitcher with Affinity Filtration Technology  10.00% Off with Filter Auto-Ship (Ships every 4 Months)': 
+            name = 'Pitcher';
+            return name;
+            case 'Water Pitcher with Affinity Filtration Technology  10.00% Off with Filter Auto-Ship (Ships every 5 Months)': 
+            name = 'Pitcher';
+            return name;
+            case 'Water Pitcher with Affinity Filtration Technology  10.00% Off with Filter Auto-Ship (Ships every 6 Months)': 
+            name = 'Pitcher';
+            return name;
+
+            case 'Water Pitcher with Affinity Filtration Technology  10.00% Off with Filter Auto-Ship Subscription (Ships every 2 Months)': 
+            name = 'Pitcher';
+            return name;
+
+            case 'Water Pitcher with Affinity Filtration Technology  10.00% Off with Filter Auto-Ship Subscription (Ships every 3 Months)': 
+            name = 'Pitcher';
+            return name;
+
+            case 'Water Pitcher with Affinity Filtration Technology  10.00% Off with Filter Auto-Ship Subscription (Ships every 4 Months)': 
+            name = 'Pitcher';
+            return name;
+
+            case 'Water Pitcher with Affinity Filtration Technology  10.00% Off with Filter Auto-Ship Subscription (Ships every 5 Months)': 
+            name = 'Pitcher';
+            return name;
+
+            case 'Water Pitcher with Affinity Filtration Technology  10.00% Off with Filter Auto-Ship Subscription (Ships every 6 Months)': 
+            name = 'Pitcher';
+            return name;
+
+
+            
             //BUNDLE: PITCHER AND 1 FILTER
             //BUNDLE: PITCHER AND 3 FILTER
             //BUNDLE: PITCHER AND 6 FILTER
